9e8810705f3f8a92487238c57b3af239c0221289
Anton Aladin <aladin.anton95@gmail.com>
2022-09-09 02:31:34 +0700

    fix 3 module 4 task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -4,8 +4,6 @@ function showSalary(users, age) {
   array.forEach((user) => {
     if (user.age <= age) str += `${user.name}, ${user.balance}\n`;
   });
-  if (str.endsWith("\n")) {
-    str = str.slice(0, str.length - 1);
-  }
+  if (str.endsWith("\n")) str = str.slice(0, str.length - 1);
   return str;
 }
