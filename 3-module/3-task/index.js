1d8ecbc8af709dfe54766060e0b12b23b9ce51b8
Anton Aladin <aladin.anton95@gmail.com>
2022-09-12 19:05:19 +0700

    3-module 3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,10 +1,11 @@
 function camelize(str) {
-  let format = str.split("-");
-  return format.map(function (item) {
-    if (item === format[0]) {
+  return str
+    .split("-")
+    .map(function(item, index) {
+      if (index) {
+        return item.replace(item[0], item[0].toUpperCase());
+      }
       return item;
-    } else {
-      return item.replace(item[0], item[0].toUpperCase());
-    }
-  }).join('');
+    })
+    .join("");
 }
