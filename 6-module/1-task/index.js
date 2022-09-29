c4b4c9f621146df08085dcb4ebe934bd5d27e1c8
Anton Aladin <aladin.anton95@gmail.com>
2022-09-29 18:23:52 +0700

    6-module 1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,51 @@
  *
  */
 export default class UserTable {
+  elem = null;
+
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.#render();
+  }
+
+  #render() {
+    const table = document.createElement("table");
+    const tBody = document.createElement("tbody");
+    let tr = null;
+
+    table.innerHTML = `
+    <thead>
+    <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+    </tr>
+    </thead>
+    `;
+
+    this.rows.forEach((element) => {
+      tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${element.name}</td>
+        <td>${element.age}</td>
+        <td>${element.salary}</td>
+        <td>${element.city}</td>
+        <td></td>
+      `;
+      tr.lastElementChild.innerHTML = `
+        <button>X</button>
+      `;
+      tr.lastElementChild
+        .querySelector("button")
+        .addEventListener("click", function () {
+          this.closest("tr").remove();
+        });
+      tBody.append(tr);
+    });
+
+    table.append(tBody);
+    return table;
   }
 }
