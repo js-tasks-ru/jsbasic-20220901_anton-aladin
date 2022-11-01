8cb7ae9ddfcb2f6d9fb5376e2d2815832847fd72
Антон <anton@MacBook-Air-user.local>
2022-11-01 13:17:13 +0700

    8-module 2-task

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -2,8 +2,42 @@ import createElement from '../../assets/lib/create-element.js';
 import ProductCard from '../../6-module/2-task/index.js';
 
 export default class ProductGrid {
+  elem = null;
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.list = [];
+    this.prodGrid = createElement(`<div class="products-grid">
+    <div class="products-grid__inner">   
+    </div>
+    </div>`);
+    this.elem = this.#render();
+  }
+
+  #render(list = this.products){
+    this.prodGrid.querySelector('.products-grid__inner').innerHTML = '';
+    list.forEach(product => {
+      let prodCard = new ProductCard(product);
+      this.prodGrid.querySelector('.products-grid__inner').append(prodCard.elem);
+    });
+    return this.prodGrid;
+  }
+
+  updateFilter(filters){
+    Object.assign(this.filters, filters);
+    this.list = [];
+
+    for (let product of this.products) {
+      if (this.filters.noNuts && product.nuts) {continue;}
+      if (this.filters.vegeterianOnly && !product.vegeterian) {continue;}
+      if (this.filters.maxSpiciness !== undefined && product.spiciness > this.filters.maxSpiciness) {
+        continue;
+      }
+      if (this.filters.category && product.category != this.filters.category) {
+        continue;
+      }
+      this.list.push(product);
+    }
+    this.#render(this.list);
   }
 }
